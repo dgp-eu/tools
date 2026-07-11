f7117dc0458f74bf849813428737df942f9fb958
Daniel Popiniuc <DanielPopiniuc@gmail.com>
2026-07-11 12:41:07 +0300

    just formatting

--- tools-databases/src/main/resources/SQL/Snowflake/ColumnsAll.sql
+++ tools-databases/src/main/resources/SQL/Snowflake/ColumnsAll.sql
@@ -1,59 +1,53 @@
 WITH
-    "CTE__COMMON"                                                               AS (
+    "CTE__System_And_Timing"                                                    AS (
         SELECT
               CURRENT_ACCOUNT()                                                 AS "SNOWFLAKE_INSTANCE"
             , SYSDATE()                                                         AS "EXTRACTION_TIMESTAMP_UTC"
     )
 SELECT
-      "cols"."TABLE_CATALOG"                                                    AS "Database"
-    , "cols"."TABLE_SCHEMA"                                                     AS "Schema"
-    , "cols"."TABLE_NAME"                                                       AS "Object"
-    , "cols"."ORDINAL_POSITION"                                                 AS "Position"
-    , "cols"."COLUMN_NAME"                                                      AS "Column"
-    , "cols"."DATA_TYPE"                                                        AS "Data Type"
+      "metadata"."TABLE_CATALOG"                                                AS "Database"
+    , "metadata"."TABLE_SCHEMA"                                                 AS "Schema"
+    , "metadata"."TABLE_NAME"                                                   AS "Object"
+    , "metadata"."ORDINAL_POSITION"                                             AS "Position"
+    , "metadata"."COLUMN_NAME"                                                  AS "Column"
+    , "metadata"."DATA_TYPE"                                                    AS "Data Type"
     , CASE
-        WHEN "cols"."DATA_TYPE" = 'TEXT'
-            AND ("cols"."CHARACTER_MAXIMUM_LENGTH" >= 16777215)    THEN
-            "cols"."DATA_TYPE"
-        WHEN "cols"."DATA_TYPE" = 'TEXT'                           THEN
+        WHEN    "metadata"."DATA_TYPE"                  = 'TEXT'
+            AND "metadata"."CHARACTER_MAXIMUM_LENGTH"   >= 16777215         THEN
+            "metadata"."DATA_TYPE"
+        WHEN "metadata"."DATA_TYPE"                     = 'TEXT'            THEN
             'VARCHAR('
-                || TO_CHAR("cols"."CHARACTER_MAXIMUM_LENGTH")
-                || ')'
-        WHEN "cols"."DATA_TYPE" = 'FLOAT'                          THEN
-            "cols"."DATA_TYPE"
-        WHEN "cols"."DATA_TYPE" = 'NUMBER'                         THEN
+                || TO_CHAR("metadata"."CHARACTER_MAXIMUM_LENGTH") || ')'
+        WHEN "metadata"."DATA_TYPE"                     IN (  'FLOAT'
+                                                            , 'DECFLOAT')   THEN
+            "metadata"."DATA_TYPE"
+        WHEN "metadata"."DATA_TYPE"                     = 'NUMBER'          THEN
             'NUMBER('
-                || TO_CHAR("cols"."NUMERIC_PRECISION")
-                || ','
-                || TO_CHAR("cols"."NUMERIC_SCALE")
-                || ')'
-        WHEN "cols"."DATA_TYPE" IN ('ARRAY'
-            , 'BINARY'
-            , 'BOOLEAN'
-            , 'DATE'
-            , 'OBJECT'
-            , 'TIME'
-            , 'VARIANT')                                    THEN
-            "cols"."DATA_TYPE"
-        WHEN "cols"."DATA_TYPE" IN ('TIMESTAMP'
-            , 'TIMESTAMP_LTZ'
-            , 'TIMESTAMP_NTZ'
-            , 'TIMESTAMP_TZ')                               THEN
-            "cols"."DATA_TYPE"
-                || '('
-                ||  "cols"."DATETIME_PRECISION"
-                || ')'
+                || TO_CHAR("metadata"."NUMERIC_PRECISION")
+                || ',' || TO_CHAR("metadata"."NUMERIC_SCALE") || ')'
+        WHEN "metadata"."DATA_TYPE"                     IN (  'ARRAY'
+                                                            , 'BINARY'
+                                                            , 'BOOLEAN'
+                                                            , 'DATE'
+                                                            , 'MAP'
+                                                            , 'OBJECT'
+                                                            , 'TIME'
+                                                            , 'VARIANT')    THEN
+            "metadata"."DATA_TYPE"
+        WHEN "metadata"."DATA_TYPE"                     LIKE 'TIMESTAMP%'   THEN
+            "metadata"."DATA_TYPE"
+                || '(' ||  "metadata"."DATETIME_PRECISION" || ')'
         ELSE
             '???'
         END                                                                     AS "Data Type Full"
-    , "cols"."IS_NULLABLE"                                                      AS "NULLable"
-    , "cols"."COLUMN_DEFAULT"                                                   AS "Default"
-    , "cols"."COMMENT"                                                          AS "Comment"
-    , "common"."SNOWFLAKE_INSTANCE"                                             AS "SNOWFLAKE_INSTANCE"
-    , "common"."EXTRACTION_TIMESTAMP_UTC"                                       AS "EXTRACTION_TIMESTAMP_UTC"
+    , "metadata"."IS_NULLABLE"                                                  AS "NULLable"
+    , "metadata"."COLUMN_DEFAULT"                                               AS "Default"
+    , "metadata"."COMMENT"                                                      AS "Comment"
+    , "csat".*
 FROM
-    "%s"."INFORMATION_SCHEMA"."COLUMNS"                                        AS "cols"
-    INNER JOIN "CTE__COMMON"                                                    AS "common"     ON  1 = 1
+    "%s"."INFORMATION_SCHEMA"."COLUMNS"                                         AS "metadata"
+    INNER JOIN "CTE__System_And_Timing"                                         AS "csat"   ON
+        1 = 1
 WHERE
-        "cols"."TABLE_SCHEMA"   = '%s'
-    AND "cols"."TABLE_NAME"     IN('%s');
+        "metadata"."TABLE_SCHEMA"   = '%s'
+    AND "metadata"."TABLE_NAME"     IN('%s');
