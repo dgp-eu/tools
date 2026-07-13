c10e468e45b04dbfe293cb9a00226fc1101c908c
Daniel Popiniuc <DanielPopiniuc@gmail.com>
2026-07-14 00:16:47 +0300

    style refined

--- tools-databases/src/main/resources/SQL/SQLite/StatisticsTables.sql
+++ tools-databases/src/main/resources/SQL/SQLite/StatisticsTables.sql
@@ -9,6 +9,9 @@ SELECT
     , "Sequence"                                                                AS "Sequence"
     , 'color:'
         || CASE
+            WHEN    "Records" = 0
+                AND "Sequence" = 0      THEN
+                'gray;font-size:0.85rem;font-style:italic'
             WHEN "Sequence" = "Records" THEN
                 'green'
             WHEN "Sequence" = 0         THEN
